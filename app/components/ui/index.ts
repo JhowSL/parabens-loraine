ad8262a24d4ea9f47a56c90bc89a0277217db041
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-16 20:09:37 -0300

    ajuste estruturas

---
+++ app/components/ui/index.ts
@@ -0,0 +1,10 @@
+export { default as MUIComponents } from './MUI/MaterialUI'
+
+{
+  /*      
+ // Exemplo de como usar os imports agrupados 
+<MUIComponents.ThemeProvider>
+// Seu conteúdo aqui 
+</MUIComponents.ThemeProvider>
+ */
+}
